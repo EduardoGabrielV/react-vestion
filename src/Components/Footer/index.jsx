29b3420dcc05af693a5af3a617fd3c99c052e9d9
Eduardo Gabriel Vicente <eduardo.vicente@hoepers.com>
2025-09-19 21:57:24 -0300

    Correções+

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -24,7 +24,7 @@ function Footer() {
 
       <div className={styles.footer__section}>
         <h3>VestiOn</h3>
-        <p>&copy; {createdYear === actualYear ? actualYear : `${createdYear} - ${actualYear}`} VestiOn (empresa fictícia ). Todos os direitos reservados.</p>
+        <p>&copy; {createdYear === actualYear ? actualYear : `${createdYear} - ${actualYear}`} VestiOn (empresa fictícia). Todos os direitos reservados.</p>
         <a className={styles.footer__links} href="#top">Voltar ao topo</a>
       </div>
     </footer>
